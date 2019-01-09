15d97c629648ec1e6b02a0c7d6b5ad66efed8271
Ryan Brady <ryanbrady369@gmail.com>
2019-01-09 22:43:08 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+  @@all = []
+
+  def initialize(name, type)
+    @name = name
+    @type = type
+    @@all < self
+  end 
+
+  def self.all 
+    @@all
+  end 
+
+  def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end 
+  
 end
